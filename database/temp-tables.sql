bdff421c439d9dee2972a303bd207f617ba585b8
Graeme <graemefolk@gmail.com>
2022-11-26 10:34:10 -0700

    simple registeredusers table for testing server

---
+++ database/temp-tables.sql
@@ -0,0 +1,11 @@
+CREATE TABLE IF NOT EXISTS registeredusers (
+    id int NOT NULL AUTO_INCREMENT,
+    first_name varchar(255),
+    last_name varchar(255),
+    email_address varchar(255) NOT NULL UNIQUE,
+    password varchar(255),
+    address varchar(255),
+    credit_card varchar(255),
+    annual_fee_expiry_date varchar(255),
+    PRIMARY KEY (id)
+);
